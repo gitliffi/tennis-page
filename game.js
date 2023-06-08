6162efba61462833124d9fd9273a16f0326bb375
gitliffi <muhdaliffi@gmail.com>
2023-06-08 17:07:36 +0800

    copy repo

---
+++ game.js
@@ -0,0 +1,15 @@
+//Pseudocode for tennis match
+//Initialize game
+//Check with user if best of 3 or best of 5 match
+//Ask for Player A name from user
+//Ask for Player B name from user
+//Create two buttons for user to click to indicate point won by either Player
+//Create scoreboard, two rows with player names
+//Create a game score extending out of names
+//Score increments with every click
+//If tie at 40-all, adv logic applies
+//Set scores go up with every game won
+//If score ties at 6-all, initiate tie-break
+//Match ends when a player reaches score win of 2, if best of 3 chosen, or score win of 3, if best of 5 chosen
+
+//test: create a button that increments a score in a website
